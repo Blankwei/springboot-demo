c3d340a0d1e831f89d70908708678e3dae2c524b
Blankwei <592232644@qq.com>
2020-08-22 11:44:32 +0800

    redis 测试接口提交

--- springboot-idempotent/src/main/java/com/savoidage/springbootidempotent/demo/controller/SysUserController.java
+++ springboot-idempotent/src/main/java/com/savoidage/springbootidempotent/demo/controller/SysUserController.java
@@ -2,7 +2,10 @@ package com.savoidage.springbootidempotent.demo.controller;
 
 import com.savoidage.springbootidempotent.demo.entity.SysUser;
 import com.savoidage.springbootidempotent.demo.service.SysUserService;
+import com.savoidage.springbootidempotent.demo.utils.JsonUtils;
+import com.savoidage.springbootidempotent.demo.utils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,56 +22,78 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
+    @Autowired
+    private RedisUtils redisUtils;
+
+    /**
+     * redis连接测试
+     * @param redisValue
+     * @return
+     */
+    @GetMapping(value = "/test")
+    public String testRedis(@RequestParam("redisValue") String redisValue) {
+        if (!StringUtils.isEmpty(redisValue)) {
+            redisUtils.set("test", redisValue, 60L);
+        }
+        Object test = redisUtils.get("test");
+        return StringUtils.isEmpty(test) ? "nothing" : JsonUtils.jsonToObject(test.toString(), String.class);
+    }
+
     /**
      * 查询用户信息 restful get
+     *
      * @param id
      * @return
      */
     @GetMapping(value = "/sysUser/{id}")
-    public SysUser findOne(@PathVariable("id") Integer id){
+    public SysUser findOne(@PathVariable("id") Integer id) {
         return sysUserService.findOne(id);
     }
 
     /**
      * 查询用户列表 restful get
+     *
      * @return
      */
     @GetMapping(value = "/sysUser")
-    public List<SysUser> findList(){
+    public List<SysUser> findList() {
         return sysUserService.findList();
     }
 
     /**
      * 新增用户 restful post
+     *
      * @param sysUser
      * @return
      */
     @PostMapping(value = "/sysUser")
-    public String saveSysUser(@RequestBody SysUser sysUser){
+    public String saveSysUser(@RequestBody SysUser sysUser) {
         int insert = sysUserService.insert(sysUser);
-        return insert > 0 ? "success":"failed";
+        return insert > 0 ? "success" : "failed";
     }
 
     /**
      * 更新用户 restful put
+     *
      * @param sysUser
      * @return
      */
     @PutMapping(value = "/sysUser")
-    public String updateSysUser(@RequestBody SysUser sysUser){
+    public String updateSysUser(@RequestBody SysUser sysUser) {
         int update = sysUserService.update(sysUser);
-        return update > 0 ? "success":"failed";
+        return update > 0 ? "success" : "failed";
     }
 
     /**
      * 删除用户 restful delete
+     *
      * @param id
      * @return
      */
     @DeleteMapping(value = "/sysUser/{id}")
-    public String deleteSysUser(@PathVariable("id") Integer id){
+    public String deleteSysUser(@PathVariable("id") Integer id) {
         int delete = sysUserService.delete(id);
-        return delete > 0 ? "success":"failed";
+        return delete > 0 ? "success" : "failed";
     }
 
 }
